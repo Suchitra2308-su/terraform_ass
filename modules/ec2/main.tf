2ce4dd24b33f3895d4001a9a861faf1fba16fd85
VIKAS K R <vikas.kr@sigmoidanalytics.com>
2024-11-21 17:30:23 +0530

    Update main.tf
    apt changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -56,8 +56,8 @@ resource "aws_instance" "public" {
 
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              yum install -y nginx
+              apt update -y
+              apt install -y nginx
               systemctl start nginx
               systemctl enable nginx
               EOF
